fc03e4add91f7eb3a85ee4dce383d00a5d1c141b
Mikayla Lisiy <55995794+mikaylalisiy@users.noreply.github.com>
2020-08-10 23:31:24 -0400

    Add files via upload

--- mikaylastheme/wp-config.example.php
+++ mikaylastheme/wp-config.example.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'dytulcmy_idm250wp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'dytulcmy_idm250' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'idesdroseLip1' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost:8889' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -78,6 +78,9 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+
+define( 'WP_HOME', 'http://mikaylasartandmultimediaportfolio.com/idm250/' );
+define( 'WP_SITEURL', 'http://mikaylasartandmultimediaportfolio.com/idm250/' );
 
 /* That's all, stop editing! Happy publishing. */
 
