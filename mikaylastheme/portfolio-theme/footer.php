0236f6e36fa63dc3d79d627acf4ccd79896e703c
Mikayla Lisiy <55995794+mikaylalisiy@users.noreply.github.com>
2020-09-02 21:14:43 -0400

    Delete footer.php

--- mikaylastheme/portfolio-theme/footer.php
+++
@@ -1,9 +0,0 @@
-<hr></hr>
-    <footer>
-        <div class="footer">
-            <p>&copy 2020 | Mikayla Olivia Lisiy</p>
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
-</body>
-</html>
