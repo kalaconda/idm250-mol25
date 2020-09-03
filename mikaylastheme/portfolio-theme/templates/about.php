66bca13f2d3389d975953c06e778cfed8170c48e
Mikayla Lisiy <55995794+mikaylalisiy@users.noreply.github.com>
2020-09-02 21:20:27 -0400

    + templates

---
+++ mikaylastheme/portfolio-theme/templates/about.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: About
+*/
+?>
+<!-- GET HEADER -->
+<?php get_header(); ?>
+    
+    <?php get_template_part('components/about'); ?>
+        
+<!-- GET FOOTER -->
+<?php get_footer(); ?>
